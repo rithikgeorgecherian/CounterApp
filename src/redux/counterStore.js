4f968be7ed32c9d865f7f40c9d75b3926474dce2
unknown <rithikgeorgecherian7@gmail.com>
2024-10-10 14:42:53 +0530

    counter

---
+++ src/redux/counterStore.js
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+import counterSlice from './counterSlice'
+
+const counterStore = configureStore({
+    reducer:{
+        counterReducer:counterSlice
+    }
+})
+
+export default counterStore
+
+//store file
